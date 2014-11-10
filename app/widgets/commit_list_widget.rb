7891e706c76d78f13dd90f8db78023eecc6f8229
ruyielder <ruyielder@gmail.com>
2014-11-10 21:15:36 +0100

    Created CommitListWidget

---
+++ app/widgets/commit_list_widget.rb
@@ -0,0 +1,17 @@
+require 'Qt'
+require_relative 'commit_widget'
+
+
+class CommitListWidget < Qt::ListWidget
+  ITEM_HEIGHT = 100
+
+
+  def add_commit(commit)
+    item = Qt::ListWidgetItem.new(self)
+    item.setSizeHint(Qt::Size.new(0, ITEM_HEIGHT))
+    commit_widget = CommitWidget.new(self, commit)
+    add_item(item)
+    set_item_widget(item, commit_widget)
+  end
+
+end

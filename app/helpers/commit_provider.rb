6ccd990c92fdfe065c579767947efb7137dc9e75
ruyielder <ruyielder@gmail.com>
2014-11-11 21:12:27 +0100

    Integrated CommitFactory with CommitProvider

--- app/helpers/commit_provider.rb
+++ app/helpers/commit_provider.rb
@@ -1,18 +1,20 @@
 require 'rugged'
+require_relative 'commit_factory'
 
 class CommitProvider
 
-  def initialize(repository)
+  def initialize(repository, commit_factory=nil)
     @repository = repository
+    @commit_factory = commit_factory || CommitFactory.new
   end
 
   def get_commits(from_index, to_index)
     commits = []
     walker = Rugged::Walker.new(@repository)
     walker.push(@repository.last_commit)
-    walker.each_with_index do |commit, index|
+    walker.each_with_index do |rugged_commit, index|
       if from_index <= index && index < to_index
-        commits << commit
+        commits << @commit_factory.create_from_rugged_commit(rugged_commit)
       elsif index >= to_index
         return commits
       end

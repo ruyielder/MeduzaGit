b89b97f09b72805caf4e0383f73779bec7d6cf89
ruyielder <ruyielder@gmail.com>
2014-11-11 18:03:23 +0100

    Added default size to MainWindow

--- app/widgets/main_window.rb
+++ app/widgets/main_window.rb
@@ -3,10 +3,12 @@ require_relative 'list_widget_paginator_widget'
 require_relative 'commit_list_widget'
 
 class MainWindow < Qt::MainWindow
+  DEFAULT_SIZE = 300, 550
 
   def initialize(repository)
     super nil
     set_window_title 'MeduzaGit'
+    resize *DEFAULT_SIZE
 
     list_widget = CommitListWidget.new(self, repository)
     list_widget_navigator = ListWidgetPaginatorWidget.new(self, list_widget)

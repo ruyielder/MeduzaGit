2c76900a99057f476c37f2458177888eae119d0c
ruyielder <ruyielder@gmail.com>
2014-11-12 13:37:47 +0100

    Added red color & tooltip for local commits

--- app/widgets/commit_widget.rb
+++ app/widgets/commit_widget.rb
@@ -3,38 +3,42 @@ require 'Qt'
 
 class CommitWidget < Qt::Widget
 
-    def initialize(parent, commit)
-      super(parent)
-      @message_label = Qt::Label.new
-      @description_label = Qt::Label.new
-      @commit = commit
-      setup_widgets
-      setup_layout
-    end
+  def initialize(parent, commit)
+    super(parent)
+    @message_label = Qt::Label.new
+    @description_label = Qt::Label.new
+    @commit = commit
+    setup_widgets
+    setup_layout
+  end
 
-    def setup_widgets
-      @message_label.text = @commit.message
-      @message_label.font = Qt::Font.new('Verdana', 15)
-      @description_label.text = "przez #{@commit.author_name} #{@commit.humanized_time_difference}"
+  def setup_widgets
+    if @commit.is_local?
+      @message_label.style_sheet = 'QLabel { color: red }'
+      @message_label.tool_tip = 'Lokalny commit'
     end
+    @message_label.text = @commit.message
+    @message_label.font = Qt::Font.new('Verdana', 15)
+    @description_label.text = "przez #{@commit.author_name} #{@commit.humanized_time_difference}"
+  end
 
-    def setup_layout
-      layout = Qt::HBoxLayout.new(self)
-      layout.spacing = 0
-      left_layout = Qt::VBoxLayout.new
-      left_layout.spacing = 0
-      left_layout.set_contents_margins(10, 10, 10, 10)
-      left_layout.add_widget(@message_label)
-      left_layout.add_widget(@description_label)
-      layout.add_layout(left_layout)
-      set_layout(layout)
-    end
+  def setup_layout
+    layout = Qt::HBoxLayout.new(self)
+    layout.spacing = 0
+    left_layout = Qt::VBoxLayout.new
+    left_layout.spacing = 0
+    left_layout.set_contents_margins(10, 10, 10, 10)
+    left_layout.add_widget(@message_label)
+    left_layout.add_widget(@description_label)
+    layout.add_layout(left_layout)
+    set_layout(layout)
+  end
 
 
-    def paintEvent(event)
-      painter = Qt::Painter.new(self)
-      painter.drawRect(rect)
-      painter.end
-    end
+  def paintEvent(event)
+    painter = Qt::Painter.new(self)
+    painter.drawRect(rect)
+    painter.end
+  end
 
 end

6c38d6d43c13a69fe62c00e99cf785ff053cedfe
ruyielder <ruyielder@gmail.com>
2014-11-12 14:19:51 +0100

    Added @copy_sha_button to CommitWidget

--- app/widgets/commit_widget.rb
+++ app/widgets/commit_widget.rb
@@ -2,13 +2,16 @@ require 'Qt'
 
 
 class CommitWidget < Qt::Widget
+  slots :copy_commit_sha
 
   def initialize(parent, commit)
     super(parent)
     @message_label = Qt::Label.new
     @description_label = Qt::Label.new
+    @copy_sha_button = Qt::PushButton.new(self)
     @commit = commit
     setup_widgets
+    setup_slots
     setup_layout
   end
 
@@ -20,6 +23,13 @@ class CommitWidget < Qt::Widget
     @message_label.text = @commit.message
     @message_label.font = Qt::Font.new('Verdana', 15)
     @description_label.text = "przez #{@commit.author_name} #{@commit.humanized_time_difference}"
+    @copy_sha_button.text = 'SHA'
+    @copy_sha_button.maximum_size = Qt::Size.new(50, 25)
+    @copy_sha_button.tool_tip = 'Skopiuj SHA'
+  end
+
+  def setup_slots
+    connect(@copy_sha_button, SIGNAL(:clicked), self, SLOT(:copy_commit_sha))
   end
 
   def setup_layout
@@ -31,6 +41,10 @@ class CommitWidget < Qt::Widget
     left_layout.add_widget(@message_label)
     left_layout.add_widget(@description_label)
     layout.add_layout(left_layout)
+
+    right_layout = Qt::VBoxLayout.new
+    right_layout.add_widget(@copy_sha_button)
+    layout.add_layout(right_layout)
     set_layout(layout)
   end
 
@@ -39,6 +53,12 @@ class CommitWidget < Qt::Widget
     painter = Qt::Painter.new(self)
     painter.drawRect(rect)
     painter.end
+  end
+
+  def copy_commit_sha
+    $qApp.clipboard.text = @commit.sha
+    absolute_position = @copy_sha_button.map_to_global(Qt::Point.new(0, 10))
+    Qt::ToolTip::show_text(absolute_position, 'Skopiowane!')
   end
 
 end

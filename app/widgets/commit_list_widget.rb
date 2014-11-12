04eaae735da2b2da3416360dacf5d362e2203ef0
ruyielder <ruyielder@gmail.com>
2014-11-12 11:43:04 +0100

    Added humanized_time_difference to CommitWidget

--- app/widgets/commit_list_widget.rb
+++ app/widgets/commit_list_widget.rb
@@ -4,7 +4,7 @@ require_relative '../../app/helpers/commit_paginator'
 require_relative 'commit_widget'
 
 class CommitListWidget < Qt::ListWidget
-  ITEM_HEIGHT = 30
+  ITEM_HEIGHT = 60
   COMMITS_PER_PAGE = 20
 
   def initialize(parent, repository)
--- app/widgets/commit_widget.rb
+++ app/widgets/commit_widget.rb
@@ -5,13 +5,31 @@ class CommitWidget < Qt::Widget
 
     def initialize(parent, commit)
       super(parent)
+      @message_label = Qt::Label.new
+      @description_label = Qt::Label.new
       @commit = commit
-
-      set_contents_margins(10, 10, 10, 10)
-      @label = Qt::Label.new
-      @label.parent = self
-      @label.text = commit.message
+      setup_widgets
+      setup_layout
     end
+
+    def setup_widgets
+      @message_label.text = @commit.message
+      @message_label.font = Qt::Font.new('Verdana', 15)
+      @description_label.text = "przez #{@commit.author_name} #{@commit.humanized_time_difference}"
+    end
+
+    def setup_layout
+      layout = Qt::HBoxLayout.new(self)
+      layout.spacing = 0
+      left_layout = Qt::VBoxLayout.new
+      left_layout.spacing = 0
+      left_layout.set_contents_margins(10, 10, 10, 10)
+      left_layout.add_widget(@message_label)
+      left_layout.add_widget(@description_label)
+      layout.add_layout(left_layout)
+      set_layout(layout)
+    end
+
 
     def paintEvent(event)
       painter = Qt::Painter.new(self)

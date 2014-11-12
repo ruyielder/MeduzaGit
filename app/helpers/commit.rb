6b373607578a4acda0886e72c99a4020ace84f89
ruyielder <ruyielder@gmail.com>
2014-11-12 11:41:40 +0100

    Added author_name to Commit class

--- app/helpers/commit.rb
+++ app/helpers/commit.rb
@@ -1,11 +1,12 @@
 class Commit
-  attr_reader :message, :time, :humanized_time_difference
+  attr_reader :message, :author_name, :time, :humanized_time_difference
 
   def initialize(**data)
     @message = data[:message]
     @is_remote = data[:is_remote]
     @time = data[:time]
     @humanized_time_difference = data[:humanized_time_difference]
+    @author_name = data[:author_name]
   end
 
   def is_remote?
--- app/helpers/commit_factory.rb
+++ app/helpers/commit_factory.rb
@@ -6,6 +6,7 @@ class CommitFactory
   def create_from_rugged_commit(rugged_commit, is_remote)
     Commit.new(
         message: rugged_commit.message,
+        author_name: rugged_commit.author[:name],
         time: rugged_commit.time,
         humanized_time_difference: HumanizedTimeDifference.between(rugged_commit.time, Time.now),
         is_remote: is_remote

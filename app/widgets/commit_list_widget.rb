c75d9f6d54cf63dc90e35219da9ab183ab8f2bb1
ruyielder <ruyielder@gmail.com>
2014-11-12 11:10:21 +0100

    Changed ITEM_HEIGHT to 30

--- app/widgets/commit_list_widget.rb
+++ app/widgets/commit_list_widget.rb
@@ -4,7 +4,7 @@ require_relative '../../app/helpers/commit_paginator'
 require_relative 'commit_widget'
 
 class CommitListWidget < Qt::ListWidget
-  ITEM_HEIGHT = 25
+  ITEM_HEIGHT = 30
   COMMITS_PER_PAGE = 20
 
   def initialize(parent, repository)
